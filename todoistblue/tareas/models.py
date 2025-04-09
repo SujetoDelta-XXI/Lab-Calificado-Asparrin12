34315ed49caf1ebeb75d820c0da0907fe7d7ee42
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-04-09 16:50:37 -0500

    Migrates - Templates

--- todoistblue/tareas/models.py
+++ todoistblue/tareas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Actividad(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True, null=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_vencimiento = models.DateTimeField(blank=True, null=True)
+    completada = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.titulo
--- todoistblue/tareas/views.py
+++ todoistblue/tareas/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Actividad
 
-# Create your views here.
+class ActividadListView(ListView):
+    model = Actividad
+    template_name = "tareas/actividad_list.html"
+    context_object_name = "actividades"
+
+class ActividadCreateView(CreateView):
+    model = Actividad
+    fields = ["titulo", "descripcion", "fecha_vencimiento", "completada"]
+    template_name = "tareas/actividad_form.html"
+    success_url = reverse_lazy("actividad_list")
+
+class ActividadUpdateView(UpdateView):
+    model = Actividad
+    fields = ["titulo", "descripcion", "fecha_vencimiento", "completada"]
+    template_name = "tareas/actividad_form.html"
+    success_url = reverse_lazy("actividad_list")
+
+class ActividadDeleteView(DeleteView):
+    model = Actividad
+    template_name = "tareas/actividad_confirm_delete.html"
+    success_url = reverse_lazy("actividad_list")
